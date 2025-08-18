57b0e33d2831b40bfb1f8593146e7ffda7c4a6ca
HoangOanh-dev <oanht3058@gmail.com>
2025-08-18 19:56:35 +0700

    Create ProjectResource.php

---
+++ ProjectResource.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'image_url' => $this->image ? asset('storage/'. $this->image) : null,
+            'url' => $this->url,
+            'tags' => $this->tags,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
